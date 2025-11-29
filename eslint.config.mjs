a9f34285770d41e8ba7a4290e682eaa5ac894216
formationWeb-Lus <jireselusa127@gmail.com>
2025-11-29 05:36:24 -0800

    Initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,22 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  // Override default ignores of eslint-config-next
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
+    "node_modules/**",
     "next-env.d.ts",
   ]),
+  // Optional: règles personnalisées
+  {
+    rules: {
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
 ]);
-
-export default eslintConfig;
