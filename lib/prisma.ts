0f5180234e2ceaefee927148eef76374c46265f5
Alexandre Lusa <jiresselusa127@gmail.com>
2026-04-16 19:40:09 +0200

    add

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,19 +3,13 @@ import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // ton URL Render/Postgres
-  ssl: { rejectUnauthorized: false },        // nécessaire pour Render
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 const adapter = new PrismaPg(pool);
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter, // ⚠️ adapter obligatoire en Prisma 7+
-  });
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
+// ❌ PAS DE global caching ici
+export const prisma = new PrismaClient({
+  adapter,
+});
