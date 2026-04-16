ec4b5bc63b6d491c1d3d9242be994487f474a87f
Alexandre Lusa <jiresselusa127@gmail.com>
2026-04-16 19:55:03 +0200

    add

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg(pool);
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
 
-// ❌ PAS DE global caching ici
-export const prisma = new PrismaClient({
-  adapter,
-});
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
